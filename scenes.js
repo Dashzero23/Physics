e61ab5be4f827b797ced696f5b6f563f09e249a5
Dashzero23 <114184567+Dashzero23@users.noreply.github.com>
2023-05-19 11:16:39 -0700

    Add files via upload
    finish level 1, added intro, tried to do timer, doesn't work

--- scenes.js
+++ scenes.js
@@ -1,3 +1,17 @@
+class Intro extends Phaser.Scene {
+    constructor() {
+        super('intro')
+    }
+    create() {
+        this.add.text(20,50, "You are a fire spirit being captured and need to escape!").setFontSize(30);
+        this.add.text(20,100, "Don't ask me about the details I'm bad at story writing.").setFontSize(10);
+        this.input.on('pointerdown', () => {
+            this.cameras.main.fade(1000, 0,0,0);
+            this.time.delayedCall(1000, () => this.scene.start('Level 1'));
+        });
+    }
+}
+
 class level1 extends Phaser.Scene {
     constructor() {
         super("Level 1");
@@ -11,7 +25,22 @@ class level1 extends Phaser.Scene {
     }  
 
     create() {
-        let ground = this.add.sprite(500, 600, "water").setScale(3);
+        this.cameras.main.alpha = 0;
+        // Start a fade-in tween to gradually increase the alpha value to 1 (fully opaque)
+        this.tweens.add({
+        targets: this.cameras.main,
+        alpha: 1,
+        duration: 1000, // Duration of the fade-in effect in milliseconds
+        });
+
+        this.ground = this.physics.add.staticGroup();
+
+        this.ground.create(500, 600, 'water').setScale(3).refreshBody();
+
+        let instruct = this.add.text(300, 100, "Welcome to the dungeon\nUse arrow keys to move\nGet to the brown door to continue", {
+            fontSize: "28px",
+            fill: "#000000",
+        }).setOrigin(0.5);
 
         // Create a static group for the platforms
         this.platforms = this.physics.add.staticGroup();
@@ -21,7 +50,6 @@ class level1 extends Phaser.Scene {
         this.platforms.create(550, 400, 'platform').setScale(0.75).refreshBody();
         this.platforms.create(950, 250, 'platform').setScale(0.75).refreshBody();
 
-        // Optional: Set collision properties for the platforms
         this.platforms.children.iterate((platform) => {
             platform.body.checkCollision = { up: true, down: true, left: true, right: true };
         });
@@ -30,12 +58,28 @@ class level1 extends Phaser.Scene {
         this.player.setCollideWorldBounds(true);
         this.player.setBounceX(0.2);
 
-        this.physics.add.collider(this.player, this.platforms);
+        this.door = this.add.rectangle(970, 163, 70, 150, 0x8B4513); // Brown color
 
-        this.physics.add.collider(this.player, this.ground);
+        this.physics.add.existing(this.door);
+        this.door.body.allowGravity = false;
+        this.door.body.immovable = true;
+
+        // Check for collision between player and door
+        this.physics.add.overlap(this.player, this.door, () => {
+            // Transition to "Summary 1" scene when player touches the door
+            //this.scene.get('Summary 1').completionTime = completionTime;
+
+            // Transition to "Summary 1" scene when player touches the door
+            this.scene.start('Summary 1');
+        });
+
+        this.physics.add.collider(this.player, this.platforms);
+        this.physics.add.collider(this.door, this.platforms);
 
         this.cursors = this.input.keyboard.createCursorKeys();
-        this.physics.add.overlap(this.player, this.ground, this.touchWater, null, this);
+        this.physics.add.overlap(this.player, this.ground, () => {
+            this.scene.start("Level 1");
+        });
     }
 
     update() {
@@ -68,13 +112,30 @@ class level1 extends Phaser.Scene {
                 this.player.setVelocityX(160);
             }
         }
+        
+        // Get the elapsed time in seconds
+        //this.elapsed = this.timedEvent.getProgress();
+    }
+}
+
+class Summary1 extends Phaser.Scene {
+    constructor() {
+        super('Summary 1')
     }
 
-    touchWater (player, ground)
-    {
-        this.scene.start("Level 1");
+    create() {
+        this.add.text(20,100, "Don't ask me about the details I'm bad at story writing.").setFontSize(10);
+
+        // Access the completionTime value passed from the previous scene
+        const completionTime = this.scene.data.get('completionTime');
+
+        // Use the completionTime value in the next scene
+        this.add.text(20,50, "Completion time for level 1: ${completionTime} seconds.").setFontSize(30);
+        this.input.on('pointerdown', () => {
+            this.cameras.main.fade(1000, 0,0,0);
+            this.time.delayedCall(1000, () => this.scene.start('Level 1'));
+        });
     }
-    
 }
 
 // Create the Phaser game configuration
@@ -90,7 +151,7 @@ const config = {
             debug: false
         }
     },
-    scene: [level1],
+    scene: [level1, Summary1],
   };
   
   // Create the Phaser game instance
